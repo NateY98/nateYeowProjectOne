ffb54cbb71f1205f0f25d371418fec61a030d489
Nate <nate.yeow@gmail.com>
2021-03-21 16:32:46 -0400

    trying to get my click event to work

--- scripts.js
+++ scripts.js
@@ -4,4 +4,14 @@
     // when clicked again, close the nav 
 
 
-// 
+// make a variable to hold my hamburger menu
+const burger = document.getElementsByClassName("hamburger")[0];
+
+// make an event listener to check when burger menu is clicked
+ // make a function within to toggle between the class ".showNav" on the "nav.mobile"
+burger.addEventListener('click', changeClass)
+
+function changeClass(){
+    const mobileNav = document.getElementsByClassName('mobile')
+    mobileNav.classList.toggle('showNav')
+}
