98b21339e33e57b533694ea94111a4b4694b8afa
Nate <nate.yeow@gmail.com>
2021-03-21 21:48:10 -0400

    re-did my hamburger menu, making it work on toggle

--- scripts.js
+++ scripts.js
@@ -11,20 +11,12 @@ const burger = document.getElementsByClassName("hamburger")[0];
 const mobileNav = document.getElementById('mobile')
 // make an event listener to check when burger menu is clicked
 
-burger.addEventListener('click', changeClass)
-
- // make a function within to toggle between the class ".showNav" and ".hideNav" on the "nav.mobile"
-function changeClass(){    
-    // mobileNav.classList.toggle('hideNav')
-    // mobileNav.classList.toggle('showNav')
+ // make a function within to toggle between the class ".showNav" on the "nav.mobile"
+burger.addEventListener('click', function(e){
+    mobileNav.classList.toggle('showNav')
+})
 
 
-    if (mobileNav.style.display === "block"){
-        mobileNav.style.display = "none";
-    }else{
-        mobileNav.style.display = "block"
-    }
-}
 
-// I tired to make this work by using toggle styling however I was unable to figure out how to make the nav start opened. I decided to go with an if else statement to toggle the display style of the nav. I have left my failed attempt as a reference. 
+
 
